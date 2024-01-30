fd3ac6295135f740a069877e3a331c833416f91b
kmcilwain112 <81257890+kmcilwain112@users.noreply.github.com>
2024-01-30 13:26:33 -0500

    Update javascript.js
    Added media matches for responsiveness

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,19 +1,22 @@
 var modal = document.getElementById('navMenu');
 var span = document.getElementsByClassName("close")[0];
 var menuBtn = document.getElementById('modalBtn');
-var top = document.getElementById('top');
-var middle = document.getElementById('middle');
-var bottom = document.getElementById('bottom');
-function buttonPress(sect, active, border) {    
+var klc = document.querySelector("#curtain"); 
+var rv = document.querySelector("#rv");
+var fader = document.querySelector("#fader");
+var largeMobile = window.matchMedia("(max-width: 480px)");
+var tablet = window.matchMedia("(max-width: 800px)");
+/*
+   Drum Machine JS
+*/
+function buttonPress(sect, active) {    
   var p = document.getElementById(sect);
   var active1 = document.getElementById(active);
-  var b = document.getElementById(border);
   var skills =document.getElementsByClassName("skill");
-  if ( active1.style.background === 'green'){
+  if (active1.style.background === 'green'){
     active1.style.background = 'grey';          
     p.classList.remove('animated_fadeIn');
     p.style.visibility = 'hidden';
-    b.classList.remove('animated_border');
     document.querySelectorAll('.skill').forEach(item => {
     item.classList.remove('animated_skills');
 });
@@ -24,7 +27,6 @@ function buttonPress(sect, active, border) {
     active1.style.background = 'green';
     p.classList.add('animated_fadeIn');
     p.style.visibility = 'visible';
-    b.classList.add('animated_border');
     p.scrollIntoView({behavior:'smooth'});
     if ( sect === 'about'){
       document.querySelectorAll('.skill').forEach(item => {
@@ -32,30 +34,6 @@ function buttonPress(sect, active, border) {
 });
     }
   }
-
-}
-function openModal(){
-  var modal = document.getElementById('navMenu');
-  modal.classList.add('animated_fadeIn');
-  modal.style.opacity = 1;
-  modal.style.display="inline";
-  modalBtn.classList.add('rotateMenuOpenClass');
-  modalBtn.classList.remove('rotateMenuCloseClass');
-  modal.classList.remove('animated_fadeOut');
-}
-
-span.onclick = function() {
-  modal.classList.add('animated_fadeOut');
-  modal.style.opacity = 0;
-  modalBtn.classList.add('rotateMenuCloseClass');
-  modalBtn.classList.remove('rotateMenuOpenClass');
-  modal.classList.remove('animated_fadeIn');
-  modal.style.display="none";
-}
-function menuItem(sect){
-  var sect = document.getElementById(sect);
-  sect.style.visibility = 'visible';
-  sect.scrollIntoView({ behavior: "smooth"});
 }
 function buttonDown(button){
   var bt = document.getElementById(button);
@@ -66,12 +44,48 @@ function buttonUp(button){
   bt.style.boxShadow = '2px 3px black';
 }
 
-  var klc = document.querySelector("#curtain"); 
-  var rv = document.querySelector("#rv");
-  var fader = document.querySelector("#fader");
-  
+
+function lgResponsive(largeMobile) {
+  var lmFader = document.getElementById("fader");
+  if (largeMobile.matches) { // If media query matches
+    lmFader.max="63";  
+  } else {
+    lmFader.max="84";
+  }
+}
+function tabletResponsive(tablet) {
+  var tabletFader = document.getElementById("fader");
+  if (tablet.matches) { // If media query matches
+    tabletFader.max="61";  
+  } else {
+    tabletFader.max="84";
+  }
+}
+lgResponsive(largeMobile);
+tabletResponsive(tablet);
 fader.addEventListener('input', function() {
   const value = this.value;
-  klc.style.height = value + "%";
-  
+  klc.style.height = value + "%"; 
 })
+/*
+   Nav Button JS
+*/
+function openModal(){
+  var modal = document.getElementById('navMenu');
+  modal.classList.add('animated_fadeIn');
+  modal.style.opacity = 1;
+  modal.style.display="inline";
+  modal.classList.remove('animated_fadeOut');
+}
+
+span.onclick = function() {
+  modal.classList.add('animated_fadeOut');
+  modal.style.opacity = 0;
+  modal.classList.remove('animated_fadeIn');
+  modal.style.display="none";
+}
+function menuItem(sect){
+  var sect = document.getElementById(sect);
+  sect.style.visibility = 'visible';
+  sect.scrollIntoView({ behavior: "smooth"});
+}
